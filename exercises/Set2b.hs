b18bd79fda30415ba89b501efd9634d3b3200612
Sparsa <1224636+Sparsa@users.noreply.github.com>
2024-11-30 15:09:59 +0100

    added set2b.hs

--- exercises/Set2b.hs
+++ exercises/Set2b.hs
@@ -1,9 +1,8 @@
 module Set2b where
 
-import Mooc.Todo
-
 -- Some imports you'll need. Don't add other imports :)
 import Data.List
+import Mooc.Todo
 
 ------------------------------------------------------------------------------
 -- Ex 1: compute binomial coefficients using recursion. Binomial
@@ -16,7 +15,10 @@ import Data.List
 -- Hint! pattern matching is your friend.
 
 binomial :: Integer -> Integer -> Integer
-binomial = todo
+binomial n k
+  | k == 0 = 1
+  | k > 0 && n == 0 = 0
+  | otherwise = binomial (n - 1) k + binomial (n - 1) (k - 1)
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement the odd factorial function. Odd factorial is like
@@ -27,17 +29,25 @@ binomial = todo
 --   oddFactorial 6 ==> 5*3*1 ==> 15
 
 oddFactorial :: Integer -> Integer
-oddFactorial = todo
+oddFactorial n
+  | n == 1 = 1
+  | even n = oddFactorial (n - 1)
+  | otherwise = n * oddFactorial (n - 2)
 
 ------------------------------------------------------------------------------
 -- Ex 3: implement the Euclidean Algorithm for finding the greatest
 -- common divisor:
 --
 -- Given two numbers, a and b,
+
 -- * if one is zero, return the other number
+
 -- * if not, subtract the smaller number from the larger one
+
 -- * replace the larger number with this new number
+
 -- * repeat
+
 --
 -- For example,
 --   myGcd 9 12 ==> 3
@@ -56,10 +66,13 @@ oddFactorial = todo
 --   0      3
 --
 -- Background reading:
+
 -- * https://en.wikipedia.org/wiki/Euclidean_algorithm
 
 myGcd :: Integer -> Integer -> Integer
-myGcd = todo
+myGcd 0 a = a
+myGcd a 0 = a
+myGcd a b = if a > b then myGcd (a - b) b else myGcd (b - a) a
 
 ------------------------------------------------------------------------------
 -- Ex 4: Implement the function leftpad which adds space characters
@@ -71,11 +84,13 @@ myGcd = todo
 --   leftpad "xxxxx" 3 ==> "xxxxx"
 --
 -- Tips:
+
 -- * you can combine strings with the ++ operator.
+
 -- * you can compute the length of a string with the length function
 
 leftpad :: String -> Int -> String
-leftpad = todo
+leftpad s n = replicate (n - length s) ' ' ++ s
 
 ------------------------------------------------------------------------------
 -- Ex 5: let's make a countdown for a rocket! Given a number, you
@@ -86,8 +101,11 @@ leftpad = todo
 --   countdown 4 ==> "Ready! 4... 3... 2... 1... Liftoff!"
 --
 -- Hints:
+
 -- * you can combine strings with the ++ operator
+
 -- * you can use the show function to convert a number into a string
+
 -- * you'll probably need a recursive helper function
 
 countdown :: Integer -> String
@@ -109,7 +127,9 @@ countdown = todo
 -- Hint: remember the mod function!
 
 smallestDivisor :: Integer -> Integer
-smallestDivisor = todo
+smallestDivisor n = smallestDivisor' 2 n
+
+smallestDivisor' t n = if (mod n t) == 0 then t else smallestDivisor' (t + 1) n
 
 ------------------------------------------------------------------------------
 -- Ex 7: implement a function isPrime that checks if the given number
@@ -118,7 +138,10 @@ smallestDivisor = todo
 -- Ps. 0 and 1 are not prime numbers
 
 isPrime :: Integer -> Bool
-isPrime = todo
+isPrime n
+  | n == 0 || n == 1 = False
+  | smallestDivisor n == n = True
+  | otherwise = False
 
 ------------------------------------------------------------------------------
 -- Ex 8: implement a function biggestPrimeAtMost that returns the
@@ -133,4 +156,4 @@ isPrime = todo
 --   biggestPrimeAtMost 10 ==> 7
 
 biggestPrimeAtMost :: Integer -> Integer
-biggestPrimeAtMost = todo
+biggestPrimeAtMost n = if isPrime n then n else biggestPrimeAtMost (n - 1)
