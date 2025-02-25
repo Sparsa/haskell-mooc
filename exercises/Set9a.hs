cafc8c56be99e48d7d6b6d0a145ba52d8246a42b
Sparsa Roychowdhury <sparsaro@isse-67-79.isse>
2025-02-25 15:02:04 +0100

    started set9a, completed the first exercise

--- exercises/Set9a.hs
+++ exercises/Set9a.hs
@@ -26,7 +26,12 @@ import Mooc.Todo
 -- Otherwise return "Ok."
 
 workload :: Int -> Int -> String
-workload nExercises hoursPerExercise = todo
+workload nExercises hoursPerExercise
+ | total > 100 = "Holy moly!"
+ | total < 10 = "Piece of cake!"
+ | otherwise =   "Ok."
+ where
+   total = nExercises * hoursPerExercise
 
 ------------------------------------------------------------------------------
 -- Ex 2: Implement the function echo that builds a string like this:
